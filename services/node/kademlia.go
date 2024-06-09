b4ac970cc0bc7613589cf24d6648cb6dafad7747
Marioxuloh <mariosan119@gmail.com>
2024-06-09 22:50:22 +0200

    implementacion de nodo hecha

--- services/node/kademlia.go
+++ services/node/kademlia.go
@@ -5,11 +5,10 @@ import (
 	"math/bits"
 )
 
-//implementar crud sobre la tabla, para que los otros servicios puedan acceder y obtener cosas de la tabla etc etc
-
 // inicializacion de la dht
 func InitDHT(numBuckets, maxNeighborsPerBucket int) DHT {
 	// En Kademlia, generalmente se usan 160 buckets, SHA-1 es de 160 bits asi se pueden colocar cualquier ID de nodo(si tiene 1 diferencia ira en el bucket n1 . . .)
+	// en este caso yo hago un hash con sha-256 y solo cojo los primeros 20bytes(160bits)
 	buckets := make([]map[[20]byte]string, numBuckets)
 	for i := range buckets {
 		buckets[i] = make(map[[20]byte]string, maxNeighborsPerBucket)
@@ -22,6 +21,15 @@ func InitDHT(numBuckets, maxNeighborsPerBucket int) DHT {
 func (n *Node) Store(neighbor Neighbor) {
 	i := hammingDistance(n.ID, neighbor.ID)
 	n.DHT.Buckets[i][neighbor.ID] = neighbor.Address
+}
+
+func (n *Node) Delete(key [20]byte) {
+	for _, bucket := range n.DHT.Buckets {
+		if _, exists := bucket[key]; exists {
+			delete(bucket, key)
+			break
+		}
+	}
 }
 
 func (n *Node) Retrieve(key [20]byte) (string, bool) {
@@ -45,7 +53,6 @@ func (n *Node) RetrieveClosestNeighbor(targetID [20]byte) Neighbor {
 	return closestNeighbor
 }
 
-// XOR Distance calculation for 160-bit identifiers represented as [20]byte
 // Returns an integer between 0 and 159 indicating the distance
 func hammingDistance(id1, id2 [20]byte) int {
 	distance := -1
